7d048690534df8206b4181384046eadf8e086444
Kyle Tress <kyle@kyletress.com>
2016-07-07 18:23:04 -0700

    make draft picks and move to next pick

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,6 +1,6 @@
 class Draft < ApplicationRecord
   belongs_to :league
-  has_many :teams, through: :league
+  has_many :teams, -> { order 'pick ASC' }, through: :league
   has_many :draft_picks
   has_many :athletes, through: :draft_picks
 
@@ -13,23 +13,42 @@ class Draft < ApplicationRecord
     Athlete.exclude(athlete_ids)
   end
 
-  # randomize draft order
-  def randomize_draft_order
-    teams.shuffle
+  def available_picks
+    draft_picks.available
   end
 
-  # draft order
-  def draft_order
-    # get the teams
-    # rank them based on random order?
-    # at the end of each round, just reverse.
+  def next_pick
+    available_picks.first
+  end
+
+  def current_pick
+    draft_picks.where(round: self.round, pick: self.pick).first
+  end
+
+  def current_team
+    index = self.round.odd? ? (self.pick - 1) : (self.pick - self.teams.count).abs
+    teams[index]
   end
 
   def move_to_pick(draft_pick)
-    # update the draft's round and pick 
     self.round = draft_pick.round
     self.pick = draft_pick.pick
     self
+  end
+
+  def move_to_next_pick
+    self.move_to_pick(self.next_pick)
+    # self.current_pick.timestamp = Time.now huh? 
+  end
+
+  def make_pick(team, athlete)
+    pick = self.current_pick
+    if team == pick.team && self.available_athletes.any? {|athlete| athlete[:id] == athlete.id}
+      pick.athlete = athlete
+      self.current_team.draft_picks << pick
+      self.move_to_next_pick
+    end
+    pick
   end
 
 end
--- app/models/draft_pick.rb
+++ app/models/draft_pick.rb
@@ -2,4 +2,6 @@ class DraftPick < ApplicationRecord
   belongs_to :draft
   belongs_to :athlete, optional: true
   belongs_to :team
+
+  scope :available, -> {where(athlete_id: nil)}
 end
